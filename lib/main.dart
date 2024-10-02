8b1ce1dacfc093243c2cced2fe22907d67cbf422
mhmetglrq <mhmetglr.q@gmail.com>
2024-10-02 17:50:51 +0300

    Refactor project structure and add route names, convert utils, app router, box shadows, borders, and app colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:json_to_dart/injection_container.dart';
 
+import 'features/jsonToDart/presentation/bloc/dashboard_bloc.dart';
 import 'my_app.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await initializeDependencies();
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<DashboardBloc>(
+        create: (context) => sl<DashboardBloc>(),
+      )
+    ],
+    child: const MyApp(),
+  ));
 }
-
