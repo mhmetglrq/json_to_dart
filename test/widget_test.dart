3a3f897f9abadc930670e43e22a1b3788532c5fd
mhmetglrq <mhmetglr.q@gmail.com>
2024-10-02 17:51:14 +0300

    Refactor project structure and add dependencies for JSON to Dart conversion

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:json_to_dart/main.dart';
+import 'package:json_to_dart/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
